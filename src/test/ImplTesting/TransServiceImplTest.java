fe0e42a1bdd2f66c37dabf168a902f5dbcfa83e3
jasleen-kaurr <163574151+jasleen-kaurr@users.noreply.github.com>
2024-04-25 05:10:13 -0400

    Update TransServiceImplTest.java

--- src/test/ImplTesting/TransServiceImplTest.java
+++ src/test/ImplTesting/TransServiceImplTest.java
@@ -1,23 +1,16 @@
-package test.ImplTesting;
+package test;
 
-import com.shashi.beans.TransactionBean;
-import com.shashi.beans.UserBean;
 import com.shashi.constants.IUserConstants;
-import com.shashi.service.impl.OrderServiceImpl;
 import com.shashi.service.impl.TransServiceImpl;
-import com.shashi.service.impl.UserServiceImpl;
 import com.shashi.utility.DBUtil;
-import com.shashi.utility.MailMessage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import com.shashi.constants.IUserConstants;
+
 import java.sql.Timestamp;
-import java.time.Instant;
 
 import java.sql.*;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,21 +19,29 @@ class TransServiceImplTest {
   public void addTransaction(String userName, String transId, Timestamp transDateTime, double transAmount) throws Exception {
     Connection conn = DBUtil.provideConnection();
 
-    PreparedStatement ps = conn.prepareStatement("insert into transactions values(?,?,?,?)");
+    PreparedStatement ps1 = conn.prepareStatement("SET FOREIGN_KEY_CHECKS=0");
+    ps1.execute();
 
-    ps.setString(1, transId);
-    ps.setString(2, userName);
-    ps.setTimestamp(3, transDateTime);
-    ps.setDouble(4, transAmount);
+    PreparedStatement ps2 = conn.prepareStatement("insert into transactions values(?,?,?,?)");
 
-    int k = ps.executeUpdate();
+    ps2.setString(1, transId);
+    ps2.setString(2, userName);
+    ps2.setTimestamp(3, transDateTime);
+    ps2.setDouble(4, transAmount);
 
-    if (k < 0) {
+    int k2 = ps2.executeUpdate();
+
+    PreparedStatement ps3 = conn.prepareStatement("SET FOREIGN_KEY_CHECKS=1");
+    ps3.execute();
+
+    if (k2 < 0) {
       throw new Exception("did not insert successfully");
     }
 
     DBUtil.closeConnection(conn);
-    DBUtil.closeConnection(ps);
+    DBUtil.closeConnection(ps1);
+    DBUtil.closeConnection(ps2);
+    DBUtil.closeConnection(ps3);
   }
 
 
@@ -121,20 +122,29 @@ class TransServiceImplTest {
       PreparedStatement ps1 = con.prepareStatement("SET FOREIGN_KEY_CHECKS=0");
       ps1.execute();
 
-      PreparedStatement ps2 = con.prepareStatement("DELETE FROM transactions");
+      PreparedStatement ps2 = con.prepareStatement("DELETE FROM product");
       ps2.execute();
 
-      PreparedStatement ps3 = con.prepareStatement("DELETE FROM user");
+      PreparedStatement ps3 = con.prepareStatement("DELETE FROM orders");
       ps3.execute();
 
-      PreparedStatement ps4 = con.prepareStatement("SET FOREIGN_KEY_CHECKS=1");
+      PreparedStatement ps4 = con.prepareStatement("DELETE FROM transactions");
       ps4.execute();
+
+      PreparedStatement ps5 = con.prepareStatement("DELETE FROM user");
+      ps5.execute();
+
+      PreparedStatement ps6 = con.prepareStatement("SET FOREIGN_KEY_CHECKS=1");
+      ps6.execute();
 
       DBUtil.closeConnection(con);
       DBUtil.closeConnection(ps1);
       DBUtil.closeConnection(ps2);
       DBUtil.closeConnection(ps3);
       DBUtil.closeConnection(ps4);
+      DBUtil.closeConnection(ps5);
+      DBUtil.closeConnection(ps6);
+
     } catch (Exception ex) {
       System.out.println(ex.getMessage());
       System.out.println("FAILED TO CLEAN UP. CHECK SQL SERVER, DISABLE SAFE UPDATE MODE");
@@ -151,9 +161,7 @@ class TransServiceImplTest {
   @Test
   public void testGetUserIdWithExistingUser() {
     String transId = "12345";
-    Timestamp transDateTime= Timestamp.from(Instant.now());
-
-//    Timestamp transDateTime = Timestamp.valueOf("2018-09-01 09:01:15");
+    Timestamp transDateTime = Timestamp.valueOf("2018-09-01 09:01:15");
     double transAmount = 99.99;
 
     String userName = "test";
@@ -163,13 +171,6 @@ class TransServiceImplTest {
     int pinCode = 123456;
     String password = "test";
 
-//    TransactionBean transaction = new TransactionBean();
-//    transaction.setTransactionId(transId);
-//    transaction.setTransAmount(transAmount);
-//    transaction.setTransDateTime(transDateTime);
-//    transaction.setUserName(userName);
-//    OrderServiceImpl.addTransaction(transaction);
-
     try {
       assertFalse(getUserExists(emailId));
       registerUser(userName, mobileNo, emailId, address, pinCode, password);
@@ -178,12 +179,62 @@ class TransServiceImplTest {
       addTransaction(userName, transId, transDateTime, transAmount);
       assertTrue(getTransactionExists(transId));
     } catch (Exception ex) {
+      System.out.println(ex.getMessage());
       fail("failed setup");
     }
 
     TransServiceImpl transService = new TransServiceImpl();
     String res = transService.getUserId(transId);
     assertEquals(userName, res);
+
+    // cleanup and verification
+    try {
+      assertTrue(getTransactionExists(transId));
+    } catch (Exception ex) {
+      fail("failed to verify");
+    }
+  }
+
+
+  // test getUserId with multiple existing user
+  @Test
+  public void testGetUserIdWithMultipleUsers() {
+    String transId = "12345";
+    Timestamp transDateTime = Timestamp.valueOf("2018-09-01 09:01:15");
+    double transAmount = 99.99;
+
+    String userName = "test";
+    Long mobileNo = 9999999999L;
+    String emailId = "test@gmail.com";
+    String address = "test street";
+    int pinCode = 123456;
+    String password = "test";
+
+    String userName2 = "guest";
+    Long mobileNo2 = 9999999998L;
+    String emailId2 = "guest@gmail.com";
+    String address2 = "guest street";
+    int pinCode2 = 654321;
+    String password2 = "guest";
+
+    try {
+      assertFalse(getUserExists(emailId));
+      registerUser(userName, mobileNo, emailId, address, pinCode, password);
+      assertTrue(getUserExists(emailId));
+      assertFalse(getUserExists(emailId2));
+      registerUser(userName2, mobileNo2, emailId2, address2, pinCode2, password2);
+      assertTrue(getUserExists(emailId2));
+      assertFalse(getTransactionExists(transId));
+      addTransaction(userName2, transId, transDateTime, transAmount);
+      assertTrue(getTransactionExists(transId));
+    } catch (Exception ex) {
+      System.out.println(ex.getMessage());
+      fail("failed setup");
+    }
+
+    TransServiceImpl transService = new TransServiceImpl();
+    String res = transService.getUserId(transId);
+    assertEquals(userName2, res);
 
     // cleanup and verification
     try {
@@ -206,6 +257,7 @@ class TransServiceImplTest {
       addTransaction(userName, transId, transDateTime, transAmount);
       assertTrue(getTransactionExists(transId));
     } catch (Exception ex) {
+      System.out.println(ex.getMessage());
       fail("failed setup");
     }
 
